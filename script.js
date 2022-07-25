49007ba91ac0727ff35034ea93d898041169e718
leonardo <leonardommascarenhas@gmail.com>
2022-07-25 15:39:43 -0300

    Teste

--- script.js
+++ script.js
@@ -14,21 +14,42 @@ function addBookToLibrary(e) {
   myLibrary.push(book);
   createCard();
   form.reset();
+  console.log(myLibrary);
 }
 
 function createCard() {
   let container = document.querySelector(".container");
+  let cardContainer = document.createElement("div");
   let bookName = document.createElement("p");
   let bookAuthor = document.createElement("p");
   let bookPages = document.createElement("p");
-  bookName.innerHTML = `Book Name: ${myLibrary[0].name}`;
-  bookAuthor.innerHTML = `Author: ${myLibrary[0].author}`;
-  bookPages.innerHTML = `Pages: ${myLibrary[0].pages}`;
-  container.appendChild(bookName);
-  container.appendChild(bookAuthor);
-  container.appendChild(bookPages);
+  let removeBtn = document.createElement("button");
+  removeBtn.classList.add("removeBtn");
+  cardContainer.classList.add("cardContainer");
+  for (let i = 0; i < myLibrary.length; i++) {
+    bookName.innerHTML = `Book Name: ${myLibrary[i].name}`;
+    bookAuthor.innerHTML = `Author: ${myLibrary[i].author}`;
+    bookPages.innerHTML = `Pages: ${myLibrary[i].pages}`;
+    cardContainer.appendChild(bookName);
+    cardContainer.appendChild(bookAuthor);
+    cardContainer.appendChild(bookPages);
+    cardContainer.appendChild(removeBtn);
+    container.appendChild(cardContainer);
+  }
+  removeBtn.addEventListener("click", removeBookFromLibrary);
 }
 
+function removeBookFromLibrary(e) {
+  e.preventDefault();
+  let cardContainer = e.target.parentElement;
+  cardContainer.remove();
+  removeFromArray(e);
+  console.log(myLibrary);
+}
+function removeFromArray(e) {
+  index = myLibrary.indexOf(e);
+  myLibrary.splice(index, 1);
+}
 function popUp() {
   let modal = document.querySelector(".popUp");
   modal.style.display = "block";
