18027605853f3b3cf2acc3c1f5102b85352dd426
leonardo <leonardommascarenhas@gmail.com>
2022-07-27 17:54:45 -0300

    Atualizações no HTML+DOM

--- Factory Function/script.js
+++ Factory Function/script.js
@@ -1,5 +1,12 @@
-const Gameboard = (() => {
-  let gameBoard = ["", "", "", "", "", "", "", ""];
+const Gamebord = (() => {
+  let gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+  for (let i = 0; i < gameBoard.length; i++) {
+    let container = document.querySelector(".display-array");
+    let div = document.createElement("div");
+    div.classList.add("box");
+    div.innerHTML = gameBoard[i];
+    container.appendChild(div);
+  }
   return { gameBoard };
 })();
 
@@ -10,4 +17,19 @@ const player = (name, marker) => {
   };
 };
 
-const Game = (() => {})();
+const Game = (() => {
+  let playerName1 = document.querySelector("#player1");
+  let playerName2 = document.querySelector("#player2");
+  let startButton = document.getElementById("start-game");
+
+  startButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let startWindow = document.querySelector(".first-window");
+    let mainWindow = document.querySelector(".main-container");
+    startWindow.style.display = "none";
+    mainWindow.style.display = "block";
+    const player1 = player(playerName1.value, "x");
+    const player2 = player(playerName2.value, "o");
+    console.log(player1, player2);
+  });
+})();
