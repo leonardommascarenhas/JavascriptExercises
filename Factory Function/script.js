db6559d8494cff31b1330257462b9cff81f13b42
leonardo <leonardommascarenhas@gmail.com>
2022-07-29 10:39:23 -0300

    funcionalidades javascript

--- Factory Function/script.js
+++ Factory Function/script.js
@@ -1,4 +1,4 @@
-const Gamebord = (() => {
+const Gameboard = (() => {
   let gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
   for (let i = 0; i < gameBoard.length; i++) {
     let container = document.querySelector(".display-array");
@@ -21,6 +21,13 @@ const Game = (() => {
   let playerName1 = document.querySelector("#player1");
   let playerName2 = document.querySelector("#player2");
   let startButton = document.getElementById("start-game");
+  let boxes = document.querySelectorAll(".box");
+
+  let currentPlayer = "";
+
+  let switchPlayer = (player1, player2) => {
+    currentPlayer == "" ? (currentPlayer = player1) : (currentPlayer = player2);
+  };
 
   startButton.addEventListener("click", (e) => {
     e.preventDefault();
@@ -28,8 +35,19 @@ const Game = (() => {
     let mainWindow = document.querySelector(".main-container");
     startWindow.style.display = "none";
     mainWindow.style.display = "block";
-    const player1 = player(playerName1.value, "x");
-    const player2 = player(playerName2.value, "o");
-    console.log(player1, player2);
+    const player1 = player(playerName1.value, "X");
+    const player2 = player(playerName2.value, "O");
+    switchPlayer(player1, player2);
+  });
+
+  const move = (index) => {
+    if (Gameboard.gameBoard[index] === " ") {
+      Gameboard.gameBoard[index] = currentPlayer.marker;
+      boxes[index].innerHTML = currentPlayer.marker;
+      console.log(Gameboard.gameBoard);
+    }
+  };
+  boxes.forEach((box, index) => {
+    box.addEventListener("click", (e) => move(index));
   });
 })();
