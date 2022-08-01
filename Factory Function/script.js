7c51ce65117a76e916e8e537067773c865c49a59
leonardo <leonardommascarenhas@gmail.com>
2022-07-31 23:58:14 -0300

    check win function

--- Factory Function/script.js
+++ Factory Function/script.js
@@ -22,6 +22,8 @@ const Game = (() => {
   let playerName2 = document.querySelector("#player2");
   let startButton = document.getElementById("start-game");
   let boxes = document.querySelectorAll(".box");
+  let name1 = "";
+  let name2 = "";
 
   startButton.addEventListener("click", (e) => {
     e.preventDefault();
@@ -29,10 +31,12 @@ const Game = (() => {
     let mainWindow = document.querySelector(".main-container");
     startWindow.style.display = "none";
     mainWindow.style.display = "block";
+    name1 = playerName1.value;
+    name2 = playerName2.value;
   });
 
-  const player1 = player(playerName1.value, "X");
-  const player2 = player(playerName2.value, "O");
+  const player1 = player(name1, "X");
+  const player2 = player(name2, "O");
   let currentPlayer = player1;
 
   const switchPlayer = () => {
@@ -41,17 +45,81 @@ const Game = (() => {
       : (currentPlayer = player1);
   };
 
+  const checkWin = (player) => {
+    //diagonais
+    if (
+      Gameboard.gameBoard[0] == player.marker &&
+      Gameboard.gameBoard[4] == player.marker &&
+      Gameboard.gameBoard[8] == player.marker
+    ) {
+      console.log("ACERTOU");
+    }
+    if (
+      Gameboard.gameBoard[2] == player.marker &&
+      Gameboard.gameBoard[4] == player.marker &&
+      Gameboard.gameBoard[6] == player.marker
+    ) {
+      console.log("acertou");
+    }
+    //horizontais
+    if (
+      Gameboard.gameBoard[0] == player.marker &&
+      Gameboard.gameBoard[1] == player.marker &&
+      Gameboard.gameBoard[2] == player.marker
+    ) {
+      console.log("acertou");
+    }
+    if (
+      Gameboard.gameBoard[3] == player.marker &&
+      Gameboard.gameBoard[4] == player.marker &&
+      Gameboard.gameBoard[5] == player.marker
+    ) {
+      console.log("acertou");
+    }
+    if (
+      Gameboard.gameBoard[8] == player.marker &&
+      Gameboard.gameBoard[7] == player.marker &&
+      Gameboard.gameBoard[6] == player.marker
+    ) {
+      console.log("acertou");
+    }
+    //verticais
+    if (
+      Gameboard.gameBoard[8] == player.marker &&
+      Gameboard.gameBoard[5] == player.marker &&
+      Gameboard.gameBoard[2] == player.marker
+    ) {
+      console.log("acertou");
+    }
+    if (
+      Gameboard.gameBoard[0] == player.marker &&
+      Gameboard.gameBoard[3] == player.marker &&
+      Gameboard.gameBoard[6] == player.marker
+    ) {
+      console.log("acertou");
+    }
+    if (
+      Gameboard.gameBoard[7] == player.marker &&
+      Gameboard.gameBoard[4] == player.marker &&
+      Gameboard.gameBoard[1] == player.marker
+    ) {
+      console.log("acertou");
+    }
+  };
+
   const move = (index) => {
     if (Gameboard.gameBoard[index] === " ") {
       console.log(currentPlayer);
       Gameboard.gameBoard[index] = currentPlayer.marker;
       boxes[index].innerHTML = currentPlayer.marker;
+      checkWin(currentPlayer);
+      switchPlayer();
     }
   };
   boxes.forEach((box, index) => {
     box.addEventListener("click", () => {
       move(index);
-      switchPlayer();
+      console.log(currentPlayer);
     });
   });
 })();
