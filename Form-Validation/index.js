1175d84445d1bb3a932f8698cef656b55a7f3c02
leonardo <leonardommascarenhas@gmail.com>
2022-09-09 18:28:41 -0300

    almost done

--- Form-Validation/index.js
+++ Form-Validation/index.js
@@ -1,8 +1,34 @@
 const form = document.getElementById("form");
-const name = document.getElementById("name");
+const userName = document.getElementById("name");
 const zipCode = document.getElementById("zipCode");
 const password = document.getElementById("password");
 const passwordConfirmation = document.getElementById("passwordConfirmation");
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add("error");
+  inputControl.classList.remove("sucess");
+};
+const setSucess = (element) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = "";
+  inputControl.classList.add("sucess");
+  inputControl.classList.remove("error");
+};
+
+const validateInputs = () => {
+  const nameValue = userName.value;
+  const zipCodeValue = zipCode.value;
+  const passwordValue = password.value;
+  const passwordConfirmationValue = passwordConfirmation.value;
+
+  //começar daqui, aprender sobre regex;
+};
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
