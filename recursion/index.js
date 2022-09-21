0e9c73f80f88974ce7047149ea6e5680b941610d
leonardo <leonardommascarenhas@gmail.com>
2022-09-20 21:11:45 -0300

    recursion 2

--- recursion/index.js
+++ recursion/index.js
@@ -5,3 +5,12 @@ function sumRange(n, total = 0) {
   return sumRange(n - 1, total + n);
 }
 console.log(sumRange(3));
+
+function power(num1, num2) {
+  if (num2 <= 1) {
+    return num1;
+  }
+  return num1 * power(num1, num2 - 1);
+}
+
+console.log(power(2, 4));
